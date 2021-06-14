ae25b08d73eea501ba75f51c5b6e084991be4ab2
Mulyawan Sentosa <development@flazhost.com>
2021-06-14 18:50:16 +0700

    Initial Release

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,37 @@
+import axios from 'axios'
 import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    counter: 0,
+    color: 'blue'
   },
   mutations: {
+    decreaseCounter(state, number){
+      state.counter -= number
+    },
+    increaseCounter(state, number){
+      state.counter += number
+      setters.test
+    },
+    setColor(state, color){
+      state.color = color
+    }
   },
   actions: {
+    decreaseCounter({ commit }){
+      axios('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new').then(response => {
+        commit('decreaseCounter', response.data)
+      })
+    },
+    increaseCounter( {commit} ){
+      axios('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new').then(response => {
+        commit('increaseCounter', response.data)
+      })
+    },
+    setColor({commit}, color){
+      commit('setColor', color)
+    }
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="counter" :style="{ color: $store.state.color }">
+      {{ $store.state.counter }}
+    </div>
+    <class class="counter-square">
+      {{ $store.state.counter }}
+      <sup>2</sup> = 
+      {{ $store.getters.squareCounter }}
+    </class>
+    <div class="buttons">
+      <button @click="$store.dispatch('decreaseCounter')">-</button>
+      <button @click="$store.dispatch('increaseCounter')">+</button>
+    </div>
+    <div>
+      <input 
+      v-model="color"
+      type="text" 
+      placeholder="Masukkan Warna">
+    </div>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+<style>
+.counter {
+  font-size: 80px;
 }
+.buttons button {
+  font-size: 20px;
+  padding: 10px;
+  margin: 2px;
+  width: 100px;
+}
+</style>
+<script>
+  export default {
+    name: 'Home',
+    methods: {
+    },
+    computed: {
+      color: {
+        get(){
+          return this.$store.state.color
+        },
+        set(data){
+          this.$store.dispatch('setColor', data)
+        }
+      }
+    }
+  }
 </script>
